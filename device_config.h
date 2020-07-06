2b4039c273b4b9f9ffa24fedb29d061eb488a3cc
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-07-06 06:58:52 +0200

    Add device_config file
    A file that defines the device's configuration bits and also contains
    some application specific functions like ASSERT.

---
+++ device_config.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   device_config.h
+ * Author: Diaa Eldeen
+ *
+ * Created on July 5, 2020, 5:21 PM
+ */
+
+#ifndef DEVICE_CONFIG_H
+#define	DEVICE_CONFIG_H
+
+
+#define _XTAL_FREQ 16000000
+
+#define ASSERT(expr)    if(!(expr)) while(1)
+
+
+#endif	/* DEVICE_CONFIG_H */
+
