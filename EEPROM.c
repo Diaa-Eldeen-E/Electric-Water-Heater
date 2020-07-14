d05a34523b1323fede691d55c4cc872496772348
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-07-14 09:17:24 +0200

    Document EEPROM device driver

--- EEPROM.c
+++ EEPROM.c
@@ -1,17 +1,66 @@
-/* 
- * File:   EEPROM.c
- * Author: Diaa Eldeen
- *
- * Created on July 7, 2020, 5:51 PM
+/*
+* Filename:     EEPROM.c
+* Compiler:     XC8
+* Target:       PIC16F877A
+*
+* MIT License 
+*
+* Copyright (c) 2020 - Diaa Eldeen
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
+/******************************************************************************
+ * @file    EEPROM.c
+ * @brief   EEPROM device driver implementation
+ * @author  Diaa Eldeen
+ * @date    July 7, 2020
+ ******************************************************************************/
+
+/** 
+ * @addtogroup EEPROM
+ * @{
  */
 
+//*****************************************************************************
+//                                   Includes
+//*****************************************************************************
 #include "EEPROM.h"
 
-extern uint8_t tempSet;
 
-
-
-void EEPROM_writeByte(uint8_t address, uint8_t data) {
+//*****************************************************************************
+// Function Definitions
+//*****************************************************************************
+/**
+ * @brief   Write a byte to an external EEPROM.
+ * 
+ * Writes a data byte in a specific address to the I2C connected
+ * external EEPROM. 
+ * 
+ * @param      address Address to be written to inside the EEPROM.
+ * @param      data    Data to be written to the specified address.
+ * @return     none.
+ * @note    #EEPROM_DEVICE_ADDR must be defined with the correct EEPROM address
+ *          on the bus.
+ */
+void EEPROM_WriteByte(uint8_t address, uint8_t data) {
 
     I2C_masterStart();
     while (I2C_masterWrite(EEPROM_DEVICE_ADDR)) //7 bit address + Write
@@ -22,7 +71,19 @@ void EEPROM_writeByte(uint8_t address, uint8_t data) {
     I2C_masterStop();
 }
 
-uint8_t EEPROM_readByte(uint8_t address) {
+
+/**
+ * @brief   Read a byte from an external EEPROM.
+ * 
+ * Reads a data byte from a specific address in the I2C connected
+ * external EEPROM.
+ * 
+ * @param   address Address location to be read from the EEPROM.
+ * @return  The Data inside that address location.
+ * @note    #EEPROM_DEVICE_ADDR must be defined with the correct EEPROM address
+ *          on the bus.
+ */
+uint8_t EEPROM_ReadByte(uint8_t address) {
 
     uint8_t data = 0;
     I2C_masterStart();
--- EEPROM.h
+++ EEPROM.h
@@ -1,32 +1,90 @@
-/* 
- * File:   EEPROM.h
- * Author: Diaa Eldeen
+/*
+ * Filename:     EEPROM.h
+ * Compiler:     XC8
+ * Target:       PIC16F877A
  *
- * Created on July 7, 2020, 5:51 PM
+ * MIT License 
+ *
+ * Copyright (c) 2020 - Diaa Eldeen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  */
 
-#ifndef EEPROM_H
-#define	EEPROM_H
+/**
+ * @file    EEPROM.h
+ * @brief   EEPROM device driver interface
+ * @author  Diaa Eldeen
+ * @date    July 7, 2020
+ */
+ 
+/** 
+ * @addtogroup EEPROM
+ * @{
+ */
+ 
+#ifndef EEPROM_H_
+#define EEPROM_H_
 
+
+#ifdef __cplusplus	// Provide C++ Compatibility
+extern "C"{
+#endif
+
+
+//*****************************************************************************
+// Includes
+//*****************************************************************************
 #include "I2C.h"
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
 
+//*****************************************************************************
+// Preprocessor Constants
+//*****************************************************************************
     
+/**
+ * @brief The external EEPROM device address on the I2C bus.
+ * @note This must be defined to a valid address before reading or writing data.
+ */
 #define EEPROM_DEVICE_ADDR  0xa0    //10100000
+
+/**
+ * @brief The address in the EEPROM at which the temperature set value is stored
+ * 
+ * An optional value of an address in the EEPROM used in the application code
+ * to store the temperature set value.
+ */
 #define EEPROM_SET_TEMP_ADDR    1
 
-    
-    
-void EEPROM_writeByte(uint8_t address, uint8_t data);
-uint8_t EEPROM_readByte(uint8_t address);
+
+//*****************************************************************************
+// Function Prototypes
+//*****************************************************************************
+void EEPROM_WriteByte(uint8_t address, uint8_t data);
+uint8_t EEPROM_ReadByte(uint8_t address);
 
 
-#ifdef	__cplusplus
-}
+
+
+#ifdef __cplusplus	// Provide C++ Compatibility
+} // extern "C"
 #endif
 
-#endif	/* EEPROM_H */
+#endif /*EEPROM_H_*/
 
