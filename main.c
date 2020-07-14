56276f9226a4649b5f7381484e107495e9daebbc
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2020-07-14 09:23:20 +0200

    Refactor, Add documentation

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #include "ADC.h"
 #include "stateMachines.h"
 #include "EEPROM.h"
+#include "heater.h"
 
 
 
@@ -25,31 +26,31 @@ button_t onButton;
 void turnOff(void);
 
 
-extern volatile uint8_t flag100MS;
+extern volatile uint8_t flag100MS;  ///< ADC reading routine period
 
 
-uint8_t tempSet = 60;
+uint8_t tempSet;    ///< The desired temperature value
 
 
 void main(void) {
 
     TMR0_Initialize();
-    ADC_initialize();
+    ADC_Initialize();
     ledInit();
     buttonsInit();
     SSD_init();
     I2C_masterInitialize();
+    Heater_Initialize();
     
     INTCONbits.GIE = 1; // Enable global interrupts
 
-    TRISCbits.TRISC2 = 0; // Cooling element
-    TRISCbits.TRISC5 = 0; // Heating element
+    
 
-    uint8_t tempVals[10] = {0};
-    uint16_t tempAccum = 0;
-    uint8_t idx = 0;
-    uint8_t tempAvg = 0;
-    uint8_t tempCur = 0;
+    uint8_t tempVals[10] = {0}; // Holds the last 10 temperature readings
+    uint16_t tempAccum = 0; // The summation of the last 10 readings
+    uint8_t idx = 0;        // tempVals array index
+    uint8_t tempAvg = 0;    // Average of the last 10 readings
+    uint8_t tempCur = 0;    // Current temperature reading
 
 
     while (1) {
@@ -60,12 +61,12 @@ void main(void) {
             if (onButton.prevState == 0) {
                 
                 // Read the tempSet from the EEPROM
-                tempSet = EEPROM_readByte(EEPROM_SET_TEMP_ADDR);
+                tempSet = EEPROM_ReadByte(EEPROM_SET_TEMP_ADDR);
 
                 // Initialize the tempSet value if not initialized
                 if (tempSet == 0xff) {
                     tempSet = 60;
-                    EEPROM_writeByte(EEPROM_SET_TEMP_ADDR, tempSet);
+                    EEPROM_WriteByte(EEPROM_SET_TEMP_ADDR, tempSet);
                 }
                 
                 onButton.prevState = 1;
@@ -77,7 +78,7 @@ void main(void) {
                 // Subtract the tenth read before the current one
                 tempAccum -= tempVals[idx]; 
                 
-                tempCur = ADC_readTemp();
+                tempCur = ADC_ReadTemp();
                 
                 // Add the current read to the previous 9 reads
                 tempAccum += tempCur;   
